ed94934c4a03cbcb9c2024f620d05b6a424be5fa
wplin <wplin1596@163.com>
2020-04-30 10:22:49 +0800

    初始化项目

---
+++ src/main/java/com/wplin1596/jupitersdk/JupiterSdkApplication.java
@@ -0,0 +1,13 @@
+package com.wplin1596.jupitersdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JupiterSdkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JupiterSdkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wplin1596/jupitersdk/JupiterSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wplin1596.jupitersdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JupiterSdkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
